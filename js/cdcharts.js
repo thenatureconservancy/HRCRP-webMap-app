3adc76fa59914fa0bd58c3d87a6b4ad1d44eecd3
Casey Schneebeck <cschneebeck@tnc.org>
2017-08-18 12:43:42 -0600

    Initial load

---
+++ js/cdcharts.js
@@ -0,0 +1,88 @@
+function createChart(){
+	// Set global chart variables
+	Chart.defaults.global.tooltips.enabled = false;
+	Chart.defaults.global.legend.display = false;
+	
+	// mitigation pathways chart
+	var ctx = $("#pathwayChart");
+	cd.mitPathChart = new Chart(ctx, {
+	    type: 'horizontalBar',
+	    data: {
+	        labels: ["", "", "", "", "", "", "", "", "", ""],
+	        datasets: [{
+	            label: '# of Votes',
+	            data: [12, 19, 3, 1400, 8, 3, 7, 9, 10, 5],
+	            backgroundColor:"#389E41",
+	            borderWidth: 0
+	        }]
+	    },
+	    options: {
+	        scales: {
+	            xAxes: [{ ticks: { beginAtZero:true, max:40 } }]
+	        }
+	    }
+	});
+	// Commitment chart
+	var ctx1 = $("#commitmentChart");
+	var data = {
+	  labels: ["", ""],
+	  datasets: [{
+	      backgroundColor: "#247BB6",
+	      borderColor: "#247BB6",
+	      data: [0,40]
+	    }, {
+	      backgroundColor: "#49B2E3",
+	      borderColor: "#49B2E3",
+	      data: [0,60]
+	    }, {
+	      backgroundColor: "#3C454A",
+	      borderColor: "#3C454A",
+	      data: [85,0]
+	    }]
+	};
+
+	cd.mitParis = new Chart(ctx1, {
+	  type: 'bar',
+	  data: data,
+	  options: {
+	    scales: {
+	  		xAxes: [{stacked: true}],
+	    	yAxes: [{
+	      	stacked: true,
+	      	ticks: {
+	        	beginAtZero: true 
+	         }
+	      }]
+	    }
+	  }
+	});
+}
+function updateChart(){
+	var max = 0;
+	$.each(cd.maxVals,function(i,v){
+		max = max + v;
+	})
+	var high = 0;
+	$.each(cd.highVals,function(i,v){
+		high = high + v;
+	})
+	cd.mitParis.data.datasets[0].data = [0,high];
+	cd.mitParis.data.datasets[1].data = [0,max];
+	cd.mitParis.data.datasets[2].data = [cd.parisBar,0];
+	cd.mitParis.update();
+	cd.mitPathChart.data.datasets[0].data = cd.sumVals;
+	cd.mitPathChart.update();
+	$(" .pathway-label").each(function(i,v){
+		if (cd.lblArray[i] == -99){
+			$(v).html("N/A")
+		}else{
+			var y = roundTo(cd.lblArray[i], 4)
+			y = commaSeparateNumber(y)
+			if (i == 1 || i == 2 || i == 4 || i == 6 || i == 7 || i == 8 || i == 9){
+				$(v).html('<img src="images/lowCost.png" height="17px" width="17px" style="margin-top:1px; margin-right:2px;">' + y)	
+			}else{
+				$(v).html(y)	
+			}
+		}
+	})
+}			
---
+++ js/makevariables.js
@@ -0,0 +1,13 @@
+// variables
+cd = {};
+cd.url = "http://dev.services.coastalresilience.org:6080/arcgis/rest/services/NCS/NCS_CountryDashboard/MapServer";
+cd.querySource = "menu";
+cd.selectedCountry = 0;
+cd.countries = 1;
+cd.atts = [];
+cd.highArray = ["defor_high", "wfuel_high", "mangrove_high","peat_loss_high","legumes_high","optint_high","rice_high","natfor_high","peat_res_high","refor_high"];
+cd.maxArray = ["refor_max", "defor_max", "natfor_max", "peat_res_max", "peat_loss_max", "wfuel_max", "mangrove_max", "rice_max", "optint_max", "legumes_max"];
+cd.sumArray = ["refor_sum", "defor_sum", "natfor_sum", "peat_res_sum", "peat_loss_sum", "wfuel_sum", "mangrove_sum", "rice_sum", "optint_sum", "legumes_sum"];
+// cd.degArray = ["mangrove_2deg","peat_loss_2deg","legumes_2deg","optint_2deg","rice_2deg","natfor_2deg","peat_res_2deg","refor_2deg"];
+cd.country = "";
+cd.reportCountries = ["Australia", "Brazil", "Canada", "China", "Germany", "India", "Indonesia", "Kenya", "Mexico", "United States"];
